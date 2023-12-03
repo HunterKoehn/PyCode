b01b14a29bb41c1512a43d7c5603d1b7ba91a592
Hunter K <huntertkoehn@gmail.com>
2023-12-03 07:53:11 -0600

    classes and objects 01

---
+++ Practice/classes_and_objects01.py
@@ -0,0 +1,18 @@
+# x = 5
+# y = 'string'
+
+# print(type(x))
+# print(type(y))
+
+# z = y.strip()
+
+# print(z)
+
+# def func(x):
+#     return x+1
+
+# print(func(5))
+
+# str = 'Hello World!'
+
+# print(str.replace('H', 'W'))
